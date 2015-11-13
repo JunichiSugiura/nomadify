da60640528d4070b8eef5f23d97dd84792a7b70e
Luke Sugiura <jun.sugiura.jp@gmail.com>
2015-11-13 21:45:05 +0900

    add simple form for restaurant sign up

--- collections/restaurants.js
+++ collections/restaurants.js
@@ -1,1 +1,32 @@
 Restaurants = new Mongo.Collection('restaurants');
+
+RestaurantSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: 'Name'
+  },
+  tel: {
+    type: String,
+    label: 'Tel'
+  },
+  email: {
+    type: String,
+    label: 'Email'
+  },
+  owner: {
+    type: String,
+    label: 'Owner',
+    autoValue: function() {
+      return this.userId
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: 'Created At',
+    autoValue: function() {
+      return new Date()
+    }
+  }
+});
+
+Restaurants.attachSchema( RestaurantSchema );
