09705c8ef25b817a2caafc3b1fa3867f543f1f2f
Luke Sugiura <jun.sugiura.jp@gmail.com>
2015-11-13 19:54:25 +0900

    modify settings.json

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 Template.twilioButton.events({
   'click #call': function() {
-    var customerTel = Meteor.settings.phoneNumberOfJun;
-    var messageUrl = 'https://demo.twilio.com/docs/voice.xml';
+    var customerTel = Meteor.settings.public.phoneNumberOfJun;
+    var messageUrl = 'http://localhost:3030/twilio/notification-message.xml';
     
-    Meteor.call('callCustomer', [customerTel, messageUrl], function(err, result) {
+    Meteor.call('callCustomer', customerTel, messageUrl, function(err, result) {
       if (err) {
         console.log(err);
       }
