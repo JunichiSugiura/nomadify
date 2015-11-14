dd72271ee29b2b1dee396bda6ad79121ec473050
Jun Okada <jun.okada0203@gmail.com>
2015-11-14 20:48:49 +0900

    customer schema

---
+++ collections/customers.js
@@ -0,0 +1,44 @@
+Customers = new Mongo.Collection('customers');
+
+Customers.allow({
+  insert: function(userId, doc) {
+    return !!doc;
+  }
+});
+
+CustomerSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: 'Name'
+  },
+  tel: {
+    type: Number,
+    label: 'Tel'
+  },
+  numberOfPeople: {
+    type: Number,
+    label: 'Number Of People'
+  },
+  createdAt: {
+    type: Date,
+    label: 'Created At',
+    autoValue: function() {
+      return new Date()
+    },
+    autoform: {
+      type: 'hidden'
+    }
+  },
+  restaurantId: {
+    type: String,
+    label: 'Restaurant Id',
+    autoValue: function() {
+      return this.userId
+    },
+    autoform: {
+      type: 'hidden'
+    }
+  }
+});
+
+Customers.attachSchema( CustomerSchema );
